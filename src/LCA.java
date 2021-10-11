17658e3d530b7101722abcece4d7b34cdd300043
Sai-Rayanapati <kanthras@tcd.ie>
2021-10-11 20:06:52 +0100

    LCA for software engineering

---
+++ src/LCA.java
@@ -0,0 +1,67 @@
+public class LCA {
+
+    static class Node{
+        int data;
+        Node right, left;
+
+        public Node(int data)
+        {
+            this.data=data;
+            right=left=null;
+        }
+    }
+
+    public static class BinaryTree{
+        Node root;
+        Boolean nodePresent(Node root, int num){
+            if(root==null)
+            {
+                return false;
+            }
+            if(root.data==num){
+                return true;
+            }
+            if(root.left!= null && nodePresent(root.left,num)){
+                return true;
+            }
+            if(root.right != null && nodePresent(root.right,num)){
+                return true;
+            }
+            return false;
+        }
+
+        String findLCA(int n1, int n2){
+            if(!nodePresent(root,n1)||!nodePresent(root,n2))
+            {
+                return "Node not present!";
+            }
+            String data= Integer.toString(LCA(root, n1, n2).data) ;
+            return data;
+        }
+
+        Node LCA(Node root, int n1, int n2)
+        {
+                if (root == null) {
+                    return null;
+                } else if (root.data == n1 || root.data == n2) {
+                    return root;
+                }
+
+                Node left = LCA(root.left, n1, n2);
+                Node right = LCA(root.right, n1, n2);
+
+                if (left != null && right != null) {
+                    return root;
+                }
+                if (left == null) {
+                    return right;
+                } else {
+                    return left;
+                }
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/LCATest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LCATest {
+
+    @Test
+    public void LCA() {
+        LCA.BinaryTree tree = new LCA.BinaryTree();
+        tree.root = new LCA.Node(1);
+        tree.root.left = new LCA.Node(2);
+        tree.root.right = new LCA.Node(3);
+        tree.root.left.left = new LCA.Node(4);
+        tree.root.left.right = new LCA.Node(5);
+        tree.root.right.left = new LCA.Node(6);
+        tree.root.right.right = new LCA.Node(7);
+
+        // Nodes with same parent node
+        assertEquals("2", tree.findLCA(4,5));
+
+        //Nodes with different parent nodes
+        assertEquals("1", tree.findLCA(4,6));
+
+        // Nodes with different depths
+        assertEquals("1", tree.findLCA(3,4));
+
+        //Nodes as parent and child
+        assertEquals("2", tree.findLCA(2,4));
+
+        // Nodes not present
+        assertEquals("Node not present!", tree.findLCA(2,8));
+
+        //Same Nodes
+        assertEquals("1", tree.findLCA(1,1));
+    }
+}
